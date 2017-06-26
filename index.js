bb011717ffbd6eedc956708d331b7d7e4b71a688
Sofia <honey.klassen1498@gmail.com>
2017-06-26 15:37:26 +0300

    HW6_JS: add empty html,js files

---
+++ index.js
@@ -0,0 +1,9 @@
+var button = document.getElementById('watch');
+button.addEventListener('click', currentTime);
+
+function currentTime() {
+new Date();
+return Date();
+}
+
+button.addEventListener('click', currentTime);
