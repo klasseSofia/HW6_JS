a42c16020cde0f5f5fb56ace444f18844e99e627
Sofia <honey.klassen1498@gmail.com>
2017-06-26 22:36:09 +0300

    HW6_JS: add watch, and sum

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-var button = document.getElementById('watch');
-button.addEventListener('click', currentTime);
-
 function currentTime() {
-new Date();
-return Date();
+    var time = new Date();
+    var h = time.getHours(),
+        m = time.getMinutes(),
+        s = time.getSeconds();
+
+    if(h<=9) {
+        h = '0' + h;
+    }
+    if(m<=9) {
+        m = '0' + m;
+    }
+    if(s<=9) {
+        s = '0' + s;
+    }
+
+    time = h + ":" + m + ":" +s;
+    document.getElementById('watch').innerHTML=time;
+    setTimeout('currentTime()', 1000);
 }
 
-button.addEventListener('click', currentTime);
+function sum(x) {
+
+    return function(y) {
+       return x+y;
+    };
+}
+var sum1 = sum(1);
+console.log(sum1(5));
+
+
+
